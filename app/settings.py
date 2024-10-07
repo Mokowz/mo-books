efa6ea43afa623e786c117bf4f22c0c79ce452b6
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-10-07 21:03:15 +0300

    Add required hosts

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-py=*p+x+d=f#!eoddpfnls-suij@r6%p0ichi@sh$j^6*^i*2%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.210.251.220', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['3.210.251.220', '127.0.0.1', 'localhost', 'mobook.ronnykerosi.tech']
 
 
 # Application definition
