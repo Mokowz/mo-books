86da9f77597cf4a9bab2a4cc5b5aa17116ae7d40
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-10-15 17:49:49 +0300

    Integrate caching to the application

--- books/views.py
+++ books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.conf import settings
 
 from .models import (Book, Author, Genre)
 from .serializers import (
@@ -14,10 +17,18 @@ class BookList(generics.ListCreateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
 
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
+
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
 
 
 # Author
@@ -25,10 +36,18 @@ class AuthorList(generics.ListCreateAPIView):
   queryset = Author.objects.all()
   serializer_class = AuthorSerializer
 
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
+
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Author.objects.all()
   serializer_class = AuthorSerializer
+
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
 
 
 # Genre
@@ -36,7 +55,15 @@ class GenreList(generics.ListCreateAPIView):
   queryset = Genre.objects.all()
   serializer_class = GenreSerializer
 
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
+
 
 class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Genre.objects.all()
   serializer_class = GenreSerializer
+
+  @method_decorator(cache_page(settings.CACHE_TTL))
+  def dispatch(self, *args, **kwargs):
+    return super().dispatch(*args, **kwargs)
