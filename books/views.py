ab38fde54ecbeb876363bb1eb1d82483af703705
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-10-16 14:16:37 +0300

    Implement authorization techniques

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django.conf import settings
@@ -16,30 +17,36 @@ from .serializers import (
 class BookList(generics.ListCreateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
 
 
 # Author
 class AuthorList(generics.ListCreateAPIView):
   queryset = Author.objects.all()
   serializer_class = AuthorSerializer
+  permission_classes = [IsAuthenticated]
 
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Author.objects.all()
   serializer_class = AuthorSerializer
+  permission_classes = [IsAuthenticated]
 
 
 # Genre
 class GenreList(generics.ListCreateAPIView):
   queryset = Genre.objects.all()
   serializer_class = GenreSerializer
+  permission_classes = [IsAuthenticated]
 
 
 class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Genre.objects.all()
   serializer_class = GenreSerializer
+  permission_classes = [IsAuthenticated]
